d1eed4668e81e654793ac2f52abfee28943cc065
ShreyaNimal_Fliptech <flipworktech1@gmail.com>
2025-10-31 16:05:00 +0530

    change in event and session file

--- app/Events/CourseEnrolled.php
+++ app/Events/CourseEnrolled.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\Enrollment;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
@@ -11,23 +12,36 @@ class CourseEnrolled implements ShouldBroadcast
 {
     use Dispatchable, SerializesModels;
 
-    public $enrollment;
+    // Type-hint property
+    public Enrollment $enrollment;
 
-   
-    public function __construct($enrollment)
+    /**
+     * Create a new event instance.
+     *
+     * @param  Enrollment  $enrollment
+     */
+    public function __construct(Enrollment $enrollment)
     {
-        
-        $this->enrollment = $enrollment->load('student', 'course');
+        // Eager load related models
+        $this->enrollment = $enrollment->loadMissing('student', 'course');
     }
 
-    
-    public function broadcastOn()
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|\Illuminate\Broadcasting\Channel[]
+     */
+    public function broadcastOn(): PrivateChannel
     {
         return new PrivateChannel('instructor.' . $this->enrollment->course->instructor_id);
     }
 
- 
-    public function broadcastWith()
+    /**
+     * The data to broadcast.
+     *
+     * @return array
+     */
+    public function broadcastWith(): array
     {
         return [
             'enrollment_id' => $this->enrollment->id,
